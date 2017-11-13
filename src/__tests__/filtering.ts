6d458437b3c53aaa936ca948e7d7d18dfa3a088d
Aniko Litvanyi <herflis33@gmail.com>
2017-11-13 12:02:37 +0100

    ci: build fixes (#6)
    * ci: Add tsconfig.test.json
    
    * test(Fix filtering tests):

--- src/__tests__/filtering.ts
+++ src/__tests__/filtering.ts
@@ -11,30 +11,32 @@ describe('#filtering', () => {
     let repo: Mocks.MockRepository = new Mocks.MockRepository();
     beforeEach(() => {
         state = {
-            entities: {
-                contents: {
-                    5145: {
-                        DisplayName: "Duis commodo nunc",
-                        Id: 5145,
-                        Status: ["active"],
-                        Type: "Task"
+            collection: {
+                children: {
+                    entities: {
+                        5145: {
+                            DisplayName: "Duis commodo nunc",
+                            Id: 5145,
+                            Status: ["active"],
+                            Type: "Task"
+                        },
+                        5146: {
+                            DisplayName: "Maecenas nec pulvinar",
+                            Id: 5146,
+                            Status: ["active"],
+                            Type: "Task"
+                        },
+                        5147: {
+                            DisplayName: "Nulla pharetra",
+                            Id: 5147,
+                            Status: ["active"],
+                            Type: "Task"
+                        }
                     },
-                    5146: {
-                        DisplayName: "Maecenas nec pulvinar",
-                        Id: 5146,
-                        Status: ["active"],
-                        Type: "Task"
-                    },
-                    5147: {
-                        DisplayName: "Nulla pharetra",
-                        Id: 5147,
-                        Status: ["active"],
-                        Type: "Task"
-                    }
-                },
-                errorMessage: null,
-                ids: [5145, 5146, 5147],
-                isFetching: false
+                    error: null,
+                    ids: [5145, 5146, 5147],
+                    isFetching: false
+                }
             },
             listByFilter: {
                 Active: {
@@ -54,7 +56,7 @@ describe('#filtering', () => {
                 }
             }
         }
-        collection = Reducers.entities;
+        collection = Reducers.sensenet;
         myReducer = combineReducers({
             collection,
             listByFilter
@@ -64,20 +66,6 @@ describe('#filtering', () => {
             state
         );
     })
-
-    // describe('#byId reducer with filtering', () => {
-    //     it('should return the initial state', () => {
-    //         expect(Reducers.entities(undefined, {})).to.be.deep.equal({});
-    //     });
-    //     it('should handle DELETE_CONTENT_SUCCESS', () => {
-    //         const ids = [1, 2, 3];
-    //         expect(Reducers.byId(ids, { type: 'DELETE_CONTENT_SUCCESS', id: 1 })).to.be.deep.equal({ 0: 1, 2: 3 });
-    //     });
-    //     it('should return a new state with the given response', () => {
-    //         expect(Reducers.byId({}, { response: { entities: { collection: { a: 0, b: 2 } } } }))
-    //             .to.be.deep.eq({ a: 0, b: 2 });
-    //     });
-    // });
 
     describe('#getVisibleTodos', () => {
         const task = {
@@ -103,7 +91,7 @@ describe('#filtering', () => {
     });
     describe('#getErrorMessage reducer', () => {
         it('should return the initial state', () => {
-            expect(getErrorMessage(state, 'All')).to.be.eq(null);
+            expect(getErrorMessage(state, 'All')).to.be.eq(undefined);
         });
     });
     describe('#errorMessage', () => {
@@ -133,15 +121,6 @@ describe('#filtering', () => {
             store.dispatch(Actions.CreateContentSuccess(content));
             let s = store.getState();
             expect(s["listByFilter"]['Active']['ids'].length).to.be.deep.equal(4);
-        });
-        it('should return the changed array', () => {
-            content = Content.Create({
-                Path: '/Root/Sites/Default_Site/tasks',
-                Status: 'completed' as any
-            }, ContentTypes.Task, repo)
-            store.dispatch(Actions.UpdateContentSuccess(content));
-            let s = store.getState();
-            expect(s["listByFilter"]['Completed']['ids']).to.be.deep.equal([]);
         });
         it('should return the changed array', () => {
             store.dispatch(Actions.DeleteSuccess(1, 5146));
